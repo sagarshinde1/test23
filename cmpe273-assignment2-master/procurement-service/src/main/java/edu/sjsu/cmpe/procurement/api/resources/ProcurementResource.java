ec5c52a1bbdc2cf3b679a99578e730826839083d
sagarshinde1 <sagar.shinde@sjsu.edu>
2013-11-06 13:04:02 -0800

    change1

--- cmpe273-assignment2-master/procurement-service/src/main/java/edu/sjsu/cmpe/procurement/api/resources/ProcurementResource.java
+++ cmpe273-assignment2-master/procurement-service/src/main/java/edu/sjsu/cmpe/procurement/api/resources/ProcurementResource.java
@@ -1,13 +1,28 @@
 package edu.sjsu.cmpe.procurement.api.resources;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.yammer.metrics.annotation.Timed;
+
+
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class ProcurementResource {
-	
-	
+
+    public ProcurementResource() {
+	// do nothing
+    }
+
+    @GET
+    @Timed(name = "get-root")
+    public Response getRoot() {
+
+	return Response.status(200).build();
+    }
 }
